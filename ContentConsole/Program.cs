ac79b4b34027aae3b71384f34b386f5d641da2c8
Nick Meldrum <nick@nickmeldrum.com>
2016-08-02 08:49:48 +0100

    hooks up story 4 disabling to console app

--- ContentConsole/Program.cs
+++ ContentConsole/Program.cs
@@ -4,11 +4,14 @@ namespace ContentConsole
 {
     public static class Program
     {
-        public static void Main()
+        public static void Main(string [] args)
         {
             const string content = "The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting.";
 
             var reporter = new BadWordReporter(new ConsoleLogger());
+
+            if (args.Length == 1 && args[0] == "disable")
+                reporter.DisableFiltering();
 
             reporter.Report(content, new[] { "swine", "bad", "nasty", "horrible" });
 

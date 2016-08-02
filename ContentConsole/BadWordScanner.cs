014103ab1ad581e3fc1f62ae1dac4462c8d9c209
Nick Meldrum <nick@nickmeldrum.com>
2016-08-02 07:44:40 +0100

    gets console app using scanner class

--- ContentConsole/BadWordScanner.cs
+++ ContentConsole/BadWordScanner.cs
@@ -4,7 +4,30 @@
     {
         public int Scan(string phrase)
         {
-            throw new System.NotImplementedException();
+            string bannedWord1 = "swine";
+            string bannedWord2 = "bad";
+            string bannedWord3 = "nasty";
+            string bannedWord4 = "horrible";
+
+            int badWords = 0;
+            if (phrase.Contains(bannedWord1))
+            {
+                badWords = badWords + 1;
+            }
+            if (phrase.Contains(bannedWord2))
+            {
+                badWords = badWords + 1;
+            }
+            if (phrase.Contains(bannedWord3))
+            {
+                badWords = badWords + 1;
+            }
+            if (phrase.Contains(bannedWord4))
+            {
+                badWords = badWords + 1;
+            }
+
+            return badWords;
         }
     }
 }
